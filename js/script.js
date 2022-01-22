96f4e201e9de1121cf3b034121115bc0e9e0c10f
chrisfleet1 <85644957+chrisfleet1@users.noreply.github.com>
2022-01-22 16:48:51 +0000

    updated js file - changed switch code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Generates random number to randomise the messages
 function generateRandNumber (num) {
     return Math.floor(Math.random() * num)
-};
+}
 
 // Define the objects
 const collectiveChoice = {
@@ -9,10 +9,10 @@ const collectiveChoice = {
     main: ["Steak and Chips", "Southern Fried Chicken Burger and Chips", "Sesame Chicken and Skinny Fries", "Deep Fried Pizza with Onion Rings and Wedges"],
     dessert: ["Profiteroles", "Mint Aero Sundae", "Eton Mess", "Chocolate Brownie"],
     drink: ["Glass of White Wine", "Glass of Red Wine", "Pint of Peroni", "Pint of Pepsi Max"],
-};
+}
 
 // Store the choice in an array
-let personalChoice = [];
+let personalChoice = []
 
 // Iterate over the object
 
@@ -21,27 +21,27 @@ for(let prop in collectiveChoice) {
 
 // use the object's properties to customize the message being added to personalWisdom  
 
-switch (prop) {
+switch(prop) {
     case 'starter':
-        personalChoice.push(`For starter, I will have "${starter[prop][choiceIdx]}".`)
+        personalChoice.push(`For starter, I will have: ${collectiveChoice[prop][choiceIdx]}.`)
         break
     case 'main':
-        personalChoice.push(`For main, I will have "${main[prop][choiceIdx]}".`)
+        personalChoice.push(`For main, I will have: ${collectiveChoice[prop][choiceIdx]}.`)
         break
     case 'dessert':
-        personalChoice.push(`For dessert, I will have "${dessert[prop][choiceIdx]}".`)
+        personalChoice.push(`For dessert, I will have: ${collectiveChoice[prop][choiceIdx]}.`)
         break
     case 'drink':
-        personalChoice.push(`And I will have a "${drink[prop][choiceIdx]}" to drink.`)
+        personalChoice.push(`And I will have a ${collectiveChoice[prop][choiceIdx]}" to drink.`)
         break
     default:
         personalChoice.push('More time needed.')
     }
-};
+}
 
 function formatChoice(choices) {
     const formatted = personalChoice.join("\n")
     console.log(formatted)
-};
+}
 
 formatChoice(personalChoice);
